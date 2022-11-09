6b3daafb21c8caa39db5a59d53743f0e04e3be29
Никита Зуев <atomshermental@MacBook-Air-Nikita.local>
2022-11-09 09:49:58 +0300

    Initial commit

---
+++ src/main/java/com/example/ppcolloquium/PpColloquiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppcolloquium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpColloquiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpColloquiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ppcolloquium/PpColloquiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppcolloquium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpColloquiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
